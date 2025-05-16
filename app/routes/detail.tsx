344fa38e2f6b283f769196715c8ae1508fe9f94f
krisna <krisna.w2010@gmail.com>
2025-05-16 13:32:20 +0800

    Add BottomNav and Breadcrumb components; refactor Detail component to use Breadcrumb; update Layout to include BottomNav

--- app/routes/detail.tsx
+++ app/routes/detail.tsx
@@ -1,266 +1,60 @@
 import type {Route} from "./+types/detail";
-import {cafes} from "~/cafes";
 
-import {useState} from 'react'
-import {StarIcon} from '@heroicons/react/20/solid'
-import {Radio, RadioGroup} from '@headlessui/react'
-import {cn} from "~/lib/utils";
-
-const product = {
-    name: 'Basic Tee 6-Pack',
-    price: '$192',
-    href: '#',
-    breadcrumbs: [
-        { id: 1, name: 'Men', href: '#' },
-        { id: 2, name: 'Clothing', href: '#' },
-    ],
-    images: [
-        {
-            src: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-            alt: 'Two each of gray, white, and black shirts laying flat.',
-        },
-        {
-            src: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg',
-            alt: 'Model wearing plain black basic tee.',
-        },
-        {
-            src: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg',
-            alt: 'Model wearing plain gray basic tee.',
-        },
-        {
-            src: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-featured-product-shot.jpg',
-            alt: 'Model wearing plain white basic tee.',
-        },
-    ],
-    colors: [
-        { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-        { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-        { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-    ],
-    sizes: [
-        { name: 'XXS', inStock: false },
-        { name: 'XS', inStock: true },
-        { name: 'S', inStock: true },
-        { name: 'M', inStock: true },
-        { name: 'L', inStock: true },
-        { name: 'XL', inStock: true },
-        { name: '2XL', inStock: true },
-        { name: '3XL', inStock: true },
-    ],
-    description:
-        'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    highlights: [
-        'Hand cut and sewn locally',
-        'Dyed with our proprietary colors',
-        'Pre-washed & pre-shrunk',
-        'Ultra-soft 100% cotton',
-    ],
-    details:
-        'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-}
-const reviews = { href: '#', average: 4, totalCount: 117 }
+import {Breadcrumb} from "~/components/breadcrumb";
+import {db} from "../../database/db";
+import {cafes} from "../../database/schema";
+import {eq} from "drizzle-orm";
 
 export async function loader({params} : Route.LoaderArgs) {
     const cafeId = params.cafeId;
-    const cafe = cafes.find(cafe => cafe.id === parseInt(cafeId));
-    if (!cafe) {
-        throw new Error('Cafe not found');
-    }
-    return {cafe};
+    const cafe = await db.select()
+        .from(cafes).where(eq(cafes.id, parseInt(cafeId)));
+    return {cafe: cafe[0]};
 }
 
 
 export default function Detail({loaderData} : Route.ComponentProps) {
-    const [selectedColor, setSelectedColor] = useState(product.colors[0])
-    const [selectedSize, setSelectedSize] = useState(product.sizes[2])
 
     const {cafe} = loaderData;
-    console.log(cafe);
 
     return (
-        <div className="bg-white">
+        <div>
             <div className="pt-6">
-                <nav aria-label="Breadcrumb">
+                <Breadcrumb cafe={cafe} />
 
-                    <div className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                        <div className="flex items-center">
-                            <a href="/" className="mr-2 text-sm font-medium text-gray-900">
-                                Back
-                            </a>
-                        </div>
-                    </div>
-                </nav>
 
-                {/* Image gallery */}
-                <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-
-                    {cafe.images?.map((image, index) => (
-                        <img
-                            alt={cafe.name}
-                            src={image}
-                            className="hidden size-full rounded-lg object-cover lg:block"
-                        />
-                    ))}
-                </div>
-
-                {/* Product info */}
-                <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
-                    <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{cafe.name}</h1>
+                <div className="px-10">
+                    {/* Image gallery */}
+                    <div className="grid grid-cols-2 mx-auto max-w-xl gap-2">
+                        {[
+                            { index: 0, roundedClass: 'rounded-tl-md' }, // top-left
+                            { index: 1, roundedClass: 'rounded-tr-md' }, // top-right
+                            { index: 2, roundedClass: 'rounded-bl-md' }, // bottom-left
+                            { index: 3, roundedClass: 'rounded-br-md' }  // bottom-right
+                        ].map(({ index, roundedClass }) => (
+                            <div key={index}>
+                                <img
+                                    className={roundedClass}
+                                    src="https://jtkdr9ubvq.ufs.sh/f/sieYGlSKznV8Cb7RkhovGw3skSHVI9LdUuynRKe4QptXxrfg"
+                                    alt={`Cafe image from Unsplash`}
+                                />
+                            </div>
+                        ))}
                     </div>
 
-                    {/* Options */}
-                    <div className="mt-4 lg:row-span-3 lg:mt-0">
-                        <h2 className="sr-only">Product information</h2>
-                        <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
-
-                        {/* Reviews */}
-                        <div className="mt-6">
-                            <h3 className="sr-only">Reviews</h3>
-                            <div className="flex items-center">
-                                <div className="flex items-center">
-                                    {[0, 1, 2, 3, 4].map((rating) => (
-                                        <StarIcon
-                                            key={rating}
-                                            aria-hidden="true"
-                                            className={cn(
-                                                reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
-                                                'size-5 shrink-0',
-                                            )}
-                                        />
-                                    ))}
-                                </div>
-                                <p className="sr-only">{reviews.average} out of 5 stars</p>
-                                <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                    {reviews.totalCount} reviews
-                                </a>
-                            </div>
-                        </div>
-
-                        <form className="mt-10">
-                            {/* Colors */}
-                            <div>
-                                <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                                <fieldset aria-label="Choose a color" className="mt-4">
-                                    <RadioGroup value={selectedColor} onChange={setSelectedColor} className="flex items-center gap-x-3">
-                                        {product.colors.map((color) => (
-                                            <Radio
-                                                key={color.name}
-                                                value={color}
-                                                aria-label={color.name}
-                                                className={cn(
-                                                    color.selectedClass,
-                                                    'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-hidden data-checked:ring-2 data-focus:data-checked:ring-3 data-focus:data-checked:ring-offset-1',
-                                                )}
-                                            >
-                        <span
-                            aria-hidden="true"
-                            className={cn(color.class, 'size-8 rounded-full border border-black/10')}
-                        />
-                                            </Radio>
-                                        ))}
-                                    </RadioGroup>
-                                </fieldset>
-                            </div>
-
-                            {/* Sizes */}
-                            <div className="mt-10">
-                                <div className="flex items-center justify-between">
-                                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                                    <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                        Size guide
-                                    </a>
-                                </div>
-
-                                <fieldset aria-label="Choose a size" className="mt-4">
-                                    <RadioGroup
-                                        value={selectedSize}
-                                        onChange={setSelectedSize}
-                                        className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4"
-                                    >
-                                        {product.sizes.map((size) => (
-                                            <Radio
-                                                key={size.name}
-                                                value={size}
-                                                disabled={!size.inStock}
-                                                className={cn(
-                                                    size.inStock
-                                                        ? 'cursor-pointer bg-white text-gray-900 shadow-xs'
-                                                        : 'cursor-not-allowed bg-gray-50 text-gray-200',
-                                                    'group relative flex items-center justify-center rounded-md border px-4 py-3 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-hidden data-focus:ring-2 data-focus:ring-indigo-500 sm:flex-1 sm:py-6',
-                                                )}
-                                            >
-                                                <span>{size.name}</span>
-                                                {size.inStock ? (
-                                                    <span
-                                                        aria-hidden="true"
-                                                        className="pointer-events-none absolute -inset-px rounded-md border-2 border-transparent group-data-checked:border-indigo-500 group-data-focus:border"
-                                                    />
-                                                ) : (
-                                                    <span
-                                                        aria-hidden="true"
-                                                        className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                                                    >
-                            <svg
-                                stroke="currentColor"
-                                viewBox="0 0 100 100"
-                                preserveAspectRatio="none"
-                                className="absolute inset-0 size-full stroke-2 text-gray-200"
-                            >
-                              <line x1={0} x2={100} y1={100} y2={0} vectorEffect="non-scaling-stroke" />
-                            </svg>
-                          </span>
-                                                )}
-                                            </Radio>
-                                        ))}
-                                    </RadioGroup>
-                                </fieldset>
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
-                            >
-                                Add to bag
-                            </button>
-                        </form>
-                    </div>
-
-                    <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pr-8 lg:pb-16">
-                        {/* Description and details */}
-                        <div>
-                            <h3 className="sr-only">Description</h3>
-
-                            <div className="space-y-6">
-                                <p className="text-base text-gray-900">{product.description}</p>
-                            </div>
-                        </div>
-
-                        <div className="mt-10">
-                            <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-
-                            <div className="mt-4">
-                                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                                    {product.highlights.map((highlight) => (
-                                        <li key={highlight} className="text-gray-400">
-                                            <span className="text-gray-600">{highlight}</span>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="mt-10">
-                            <h2 className="text-sm font-medium text-gray-900">Location:</h2>
-
-                            <div className="mt-4 space-y-6">
-                                <p className="text-sm text-gray-600">{cafe.address}</p>
-                            </div>
+                    {/* Product info */}
+                    <div className="mt-10">
+                        <div className="mx-auto lg:mx-0">
+                            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                                {cafe.name}
+                            </h2>
+                            <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
                         </div>
                     </div>
                 </div>
-            </div>
+
+
+           </div>
         </div>
     )
 }
--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,11 +1,13 @@
 import {Outlet} from "react-router";
+import {BottomNav} from "~/components/bottom-nav";
 
 export default function Layout() {
     return (
         <div className="flex flex-col bg-white overflow-hidden">
-            <div>
-                <Outlet />
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-28">
+                <Outlet/>
             </div>
+            <BottomNav />
         </div>
     )
 }
