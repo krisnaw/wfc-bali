6fb4d4a33394130736c060f7d64f0c555b2b5dec
krisna <krisna.w2010@gmail.com>
2025-05-17 15:43:27 +0800

    Add Label and Select components with associated subcomponents for improved UI

--- app/components/areas-filter.tsx
+++ app/components/areas-filter.tsx
@@ -1,8 +1,11 @@
 import {Button} from "~/components/ui/button";
-import {Link} from "react-router";
+import {Link, useNavigation} from "react-router";
 import type {AreasModel} from "../../database/schema";
+import {Loader2} from "lucide-react";
 
 export function AreasFilter({areas}: {areas: AreasModel[]}) {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
     return (
         <div className="space-x-2 flex items-center mt-6">
             <Button variant="outline" className="capitalize">
@@ -14,6 +17,7 @@ export function AreasFilter({areas}: {areas: AreasModel[]}) {
                 <Button key={area.id} variant="outline" className="capitalize">
                     <Link to={`/${area.name}/search`} viewTransition prefetch="intent">
                         {area.name}
+                        {isNavigating && navigation.location?.pathname === `/${area.name}` &&  <Loader2 className="animate-spin" />}
                     </Link>
                 </Button>
             ))}
