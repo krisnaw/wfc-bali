9cc8f47c74d4c79baa837377762beecf7718fa15
krisna <krisna.w2010@gmail.com>
2025-05-18 20:12:02 +0800

    Add ScrollArea and ScrollBar components for enhanced scrolling functionality

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -4,7 +4,7 @@ import {BottomNav} from "~/components/bottom-nav";
 export default function Layout() {
     return (
         <div className="flex flex-col bg-white overflow-hidden">
-            <div className="mx-auto max-w-lg">
+            <div className="mx-auto max-w-lg px-4 sm:px-6 lg:px-8">
                 <Outlet/>
             </div>
             <BottomNav />
